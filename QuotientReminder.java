c071bf27c8cb0d1f507f8c11aa3f13660f698bfb
Jelin123 <70517085+Jelin123@users.noreply.github.com>
2023-07-13 23:13:17 +0530

    Add files via upload

---
+++ QuotientReminder.java
@@ -0,0 +1,15 @@
+package com.javabasicopenLabBook;
+
+public class QuotientReminder {
+	
+	public static void main(String[] args) {
+		 int dividend = 34, divisor = 4;
+		 
+		 int quotient = dividend/divisor;
+		 int remainder = dividend%divisor;
+		 
+		 System.out.println("Quotient is "+quotient);
+		 System.out.println("Remainder is "+remainder);
+	}
+
+}
