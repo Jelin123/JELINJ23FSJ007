dad64a969554931b3239550d90f9d313e7bd693e
Jelin123 <70517085+Jelin123@users.noreply.github.com>
2023-07-13 22:57:39 +0530

    Add files via upload

---
+++ CastingDemo.java
@@ -0,0 +1,19 @@
+package com.javabasicopenLabBook;
+
+public class CastingDemo {
+	
+	public static void main(String[] args) {
+		
+		byte b =11;
+		byte c = b;
+		
+		byte d = (byte)b;
+		
+		byte x=11;
+		byte y=20;
+		int mul = x*y;
+		
+		System.out.println(mul);
+	}
+
+}
