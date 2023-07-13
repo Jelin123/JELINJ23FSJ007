7c88a7eeb8c7441b3e7f502225911f071a0bfdbd
Jelin123 <70517085+Jelin123@users.noreply.github.com>
2023-07-13 22:40:16 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,16 @@
+package com.javabasicopenLabBook;
+
+public class Addition {
+	public static void main(String[] args) {
+		
+		int first = 100;
+		int second = 200;
+		
+		System.out.println(first+" "+second);
+		
+		int sum = first+second;
+		
+		System.out.println("The sum is "+sum);
+	}
+
+}
