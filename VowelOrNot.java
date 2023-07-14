8426b713cc7754c14334ce12bfa252656972e874
Jelin123 <70517085+Jelin123@users.noreply.github.com>
2023-07-14 18:27:20 +0530

    Add files via upload

---
+++ VowelOrNot.java
@@ -0,0 +1,17 @@
+package com.javabasicopenLabBook;
+
+public class VowelOrNot {
+	
+	public static void main(String[] args) {
+		 char ch = 'i';
+		 
+		 if (ch =='a'|| ch == 'e'|| ch == 'i'|| ch == 'o' || ch == 'u') {
+			 System.out.println( ch +" is vowel");
+		 }
+			 else {
+				 System.out.println(ch +"not a vowel");
+			 }
+		 }
+	}
+
+
